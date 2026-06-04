0c1ddf682647314c7cd045553b7a3656347d30d3
LiashenkoMA <makcim_an@mail.ru>
2026-06-04 19:25:38 +0400

    fix: страница повторения кандзи

--- flashcard-jp-frontend/src/app/(auth)/(jp)/kanji/repeat/page.tsx
+++ flashcard-jp-frontend/src/app/(auth)/(jp)/kanji/repeat/page.tsx
@@ -4,51 +4,7 @@ import KandjiRepeatPageComponent from "@/_components/KandjiRepeatPageComponent/K
 import { getKanji } from "@/_utils/api/server/kanjiApi";
 
 export default async function KandziRepeat() {
-  /* const kanji: IKanji[] = await getKanji(); */
-
-  const data = [
-    {
-      _id: "1",
-      kanji: "日",
-      translate: "день, солнце",
-      jpRead: "ひ、び、か",
-      chinaRead: "ニチ、ジツ",
-      learned: false,
-    },
-    {
-      _id: "2",
-      kanji: "月",
-      translate: "месяц, луна",
-      jpRead: "つき",
-      chinaRead: "ゲツ、ガツ",
-      learned: true,
-    },
-    {
-      _id: "3",
-      kanji: "火",
-      translate: "огонь",
-      jpRead: "ひ",
-      chinaRead: "カ",
-      learned: false,
-    },
-    {
-      _id: "4",
-      kanji: "水",
-      translate: "вода",
-      jpRead: "みず",
-      chinaRead: "スイ",
-      learned: true,
-    },
-    {
-      _id: "5",
-      kanji: "木",
-      translate: "дерево",
-      jpRead: "き、こ",
-      chinaRead: "モク、ボク",
-      learned: false,
-    },
-  ];
-
+  const data: IKanji[] = await getKanji();
 
   return (
     <section className={styles.kandjirepeat}>
