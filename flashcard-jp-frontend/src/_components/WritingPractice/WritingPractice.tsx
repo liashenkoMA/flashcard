f862099531d50b2be479dafd2dc8224ae4e4218b
LiashenkoMA <makcim_an@mail.ru>
2026-07-01 23:13:33 +0400

    chore: плейсхолдер у writingpractice

--- flashcard-jp-frontend/src/_components/WritingPractice/WritingPractice.tsx
+++ flashcard-jp-frontend/src/_components/WritingPractice/WritingPractice.tsx
@@ -49,6 +49,7 @@ export default function WritingPractice({
         className={styles.writingPractice__input}
         value={value}
         name="Вариант"
+        placeholder="Перевод"
         onChange={handleChange}
       />
 
