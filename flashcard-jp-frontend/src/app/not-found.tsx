d19be64c33a5d9ae9580cc34148f1d32738442ed
LiashenkoMA <makcim_an@mail.ru>
2026-07-19 15:21:41 +0400

    refactor: 404 страница добавил роутинг на кнопку

--- flashcard-jp-frontend/src/app/not-found.tsx
+++ flashcard-jp-frontend/src/app/not-found.tsx
@@ -1,7 +1,12 @@
+"use client";
+
 import styles from "./not-found.module.scss";
 import Button from "@/_components/UI/Button/Button";
+import { useRouter } from "next/navigation";
 
 export default function NotFound() {
+  const router = useRouter();
+
   return (
     <div className={styles.wrapper}>
       <main className={styles.content}>
@@ -14,13 +19,18 @@ export default function NotFound() {
               404
             </p>
             <h2 className={styles.notfound__header}>Not Found</h2>
-            <p
-              className={`${styles.notfound__text}`}
-            >
+            <p className={`${styles.notfound__text}`}>
               Страница не найдена или еще не создана. Пожалуйста, вернитесь на
               главную.
             </p>
-            <Button>Главная</Button>
+            <Button
+              type="button"
+              onClick={() => {
+                router.push("/");
+              }}
+            >
+              Главная
+            </Button>
           </div>
         </section>
       </main>
